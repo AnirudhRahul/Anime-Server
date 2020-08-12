33129b583da668392fa9d292f45727dd98bd78dd
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-12 18:39:01 -0400

    Trying send and http to check performance

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT
-app.set('view engine', 'ejs');
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT
+// app.set('view engine', 'ejs');
+// var send = require('send')
+//
+//
+// app.get('/', function(req, res) {
+//     res.render('index');
+// });
+//
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+var http = require('http')
+var parseUrl = require('parseurl')
+var send = require('send')
 
+var server = http.createServer(function onRequest (req, res) {
+  send(req, parseUrl(req).pathname, { root: './downloads' })
+    .pipe(res)
+    console.log('got request')
 
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
 })
+server.listen(process.env.PORT)
