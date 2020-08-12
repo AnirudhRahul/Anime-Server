79bc8faa07df62793f61cb4d8ccfbdca9c7065bc
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-12 01:43:43 -0400

    Updated app to use env variables

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!\t'+process.env.NODE_ENV)
 })
 
 app.listen(port, () => {
+  console.log(process.env.NODE_ENV)
   console.log(`Example app listening at http://localhost:${port}`)
 })
