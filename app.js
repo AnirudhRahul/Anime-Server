5f99a1d2c5f4edb240ab0c5fd878f99a0f0c53c4
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-12 02:40:34 -0400

    Added ability to serve static files

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!\t'+process.env.NODE_ENV)
 })
 
+app.use('/dl', express.static('downloads'))
+
+
 app.listen(port, () => {
   console.log(process.env.NODE_ENV)
   console.log(`Example app listening at http://localhost:${port}`)
