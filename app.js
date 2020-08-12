e1597e27a596c78de416b3cde30127f1a737dd3b
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-12 13:59:47 -0400

    Moved Icon to views Removed fluff from app.js

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!\t'+process.env.NODE_ENV)
-})
 
 app.use('/dl', express.static('downloads'))
 
 
-app.listen(port, () => {
-  console.log(process.env.NODE_ENV)
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!\t'+process.env.NODE_ENV)
+// })
+//
+// app.listen(port, () => {
+//   console.log(process.env.NODE_ENV)
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
