10a1bf81ab9fbc776bf4f9a2bcaf94a78a25d66c
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-16 19:12:11 -0400

    Fixing .gitignore and config files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   apps : [{
-<<<<<<< HEAD
     name: "app-prod",
     script: "./app.js",
     watch: false,
@@ -14,21 +13,6 @@ module.exports = {
     script: "./auto_torrent.js",
     env: {
       DOWNLOAD_DIR: "/usr/local/lsws/Example/data/prod"
-=======
-    name: "app-dev",
-    script: "./app.js",
-    watch: true,
-    env: {
-      NODE_ENV: "development",
-      PORT: 8001
-    },
-  },
-  {
-    name: "auto_torrent-dev",
-    script: "./auto_torrent.js",
-    env: {
-      DOWNLOAD_DIR: "/usr/local/lsws/Example/data/dev"
->>>>>>> dev
     },
   }
 ]
