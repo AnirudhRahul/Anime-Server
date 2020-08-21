89ff051b0a5c672f917df014fafe84fb053b553d
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-21 15:36:00 -0400

    Removed app.js from pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   apps : [
   {
-    name: "app-prod",
-    script: "./app.js",
-    watch: false,
-    env: {
-      NODE_ENV: "production",
-      PORT: 8000
-    },
-  },
-  {
     name: "auto_torrent-prod",
     script: "./auto_torrent.js",
     env: {
