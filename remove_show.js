8c68420bdd59c6da8e1a237b35efdfda12020a84
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-24 23:39:11 -0400

    Renamed and added console output

--- remove_show.js
+++ remove_show.js
@@ -13,6 +13,12 @@ const {root_dir, video_dir, database_dir} = require('./dirs.js').all(env)
 show_to_remove = argv.file
 remove_files = true
 files = database.removeSync(show_to_remove, database_dir)
+if(files.length>0){
+  console.log("Entry Deleted")
+}
+else {
+  console.log("Nothing Happened")
+}
 if(remove_files){
   delete_path = path.join(video_dir, show_to_remove)
   rimraf.sync(delete_path)
