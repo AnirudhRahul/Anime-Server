87da45e060c78528da69358f96ee4afed62a04f6
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-24 22:21:03 -0400

    Added program to rename mkv files to webm

---
+++ rename_mkvs.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const glob = require("glob")
+const fs = require("fs")
+const database = require('./database.js')
+
+const env = process.env.NODE_ENV || 'development';
+const {root_dir, video_dir, database_dir} = require('./dirs.js').all(env)
+
+old_ending = 'mkv'
+new_ending = 'webm'
+glob(path.join(video_dir,'**/*.'+old_ending), function (er, files) {
+  files.forEach((old_path) => {
+    new_path = old_path.substring(0,old_path.length-old_ending.length)+new_ending
+    fs.rename(old_path, new_path,()=>{})
+    console.log(new_path)
+  });
+})
+
+
+map = database.readSync(database_dir)
+// console.log(map)
+
+for (show in map) {
+  for(index in map[show]){
+    old_path = map[show][index]['video_path']
+    if(old_path.endsWith(old_ending)){
+      new_path = old_path.substring(0,old_path.length-old_ending.length)+new_ending
+      console.log(new_path)
+      map[show][index]['video_path']=new_path
+    }
+
+  }
+
+}
