0200b1d25dac418bf2bedcac26f3c8eade896705
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-24 23:13:51 -0400

    Added command line args support

---
+++ remove_show.js
@@ -0,0 +1,19 @@
+const database = require('./database.js')
+const rimraf = require("rimraf");
+const path = require("path");
+const {argv} = require('yargs')
+
+
+var env = process.env.NODE_ENV || 'development';
+if(argv.prod)
+  env = 'production'
+
+const {root_dir, video_dir, database_dir} = require('./dirs.js').all(env)
+
+show_to_remove = argv.file
+remove_files = true
+files = database.removeSync(show_to_remove, database_dir)
+if(remove_files){
+  delete_path = path.join(video_dir, show_to_remove)
+  rimraf.sync(delete_path)
+}
