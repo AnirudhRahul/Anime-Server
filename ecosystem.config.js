3d90e8581397d5b17f9426ded074b8fe931a3d06
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-26 00:28:22 -0400

    Changed config vars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
   {
     name: "auto_torrent-prod",
-    script: "./auto_torrent.js",
+    script: "./torrent/auto_torrent.js",
     env: {
       NODE_ENV: "production",
     },
