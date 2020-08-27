11b86887e463752b04354dfec4361fb2db991fc7
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-27 02:17:01 -0400

    Lowered memory limit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [
   {
     name: "auto_torrent-prod",
+    "max_memory_restart" : "500M",
     script: "./torrent/auto_torrent.js",
     env: {
       NODE_ENV: "production",
