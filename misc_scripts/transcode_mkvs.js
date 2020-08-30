5e4465f974aa4685cc72c243413d701c0763d91e
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-08-30 15:23:19 -0400

    Removed unnecesary script and renamed transcode script.

---
+++ misc_scripts/transcode_mkvs.js
@@ -0,0 +1,66 @@
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path')
+const glob = require("glob")
+const fs = require("fs")
+const database = require('../database.js')
+const {argv} = require('yargs')
+
+var env = process.env.NODE_ENV || 'development';
+if(argv.prod)
+  env = 'production'
+
+const {root_dir, video_dir, database_dir} = require('../dirs.js').all(env)
+const old_ending = 'mkv'
+const new_ending = 'mp4'
+
+glob(path.join(video_dir,'**/*.'+old_ending), function (er, files) {
+  count = 0
+  files.forEach((old_path) => {
+    if(count>0)
+      return
+    count++
+
+    new_path = changeFileEnding(old_path, new_ending)
+    subtitle_path = changeFileEnding(old_path, 'vtt')
+    console.log(new_path)
+    console.log(subtitle_path)
+    command =
+    ffmpeg()
+    .input(old_path)
+    .output(new_path)
+    .outputOptions('-c:v copy')
+    .outputOptions('-c:a copy')
+    .output(subtitle_path)
+    .on('end', function() {
+      console.log("DONE")
+      // fs.unlinkSync(old_path)
+      // console.log(new_path)
+      map = database.readSync(database_dir)
+      for (show in map) {
+        for(index in map[show]){
+          old_filename = map[show][index]['file_name']
+          if(old_path.endsWith(old_filename)){
+            map[show][index]['file_name'] = changeFileEnding(old_filename, new_ending)
+            map[show][index]['subtitle_file_name'] = path.basename(subtitle_path)
+          }
+        }
+      }
+      console.log(map)
+      // database.writeSync(map, database_dir)
+    })
+    .run()
+
+  });
+})
+
+
+function changeFileEnding(input, new_ending){
+    return input.substring(0,input.lastIndexOf('.')+1)+new_ending
+}
+// var command =
+// ffmpeg('../dev/videos/One Piece [1080p]/[HorribleSubs] One Piece - 936 [1080p].mkv')
+// .output('../dev/videos/One Piece [1080p]/[HorribleSubs] One Piece - 936 [1080p].mp4')
+// .on('end', function() {
+//   console.log('Finished processing');
+// })
+// .run()
