af39afc2c8437a16471540f8b8d95dea9f75fdc4
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-09-07 13:35:04 -0400

    Testing try catch for fallback

--- misc_scripts/transcode_mkvs.js
+++ misc_scripts/transcode_mkvs.js
@@ -46,28 +46,34 @@ module.exports.transcode_file = function transcode_file(old_path, database_dir, 
       },
       database_dir
     )
-    let thumbnail_frames = 240
     ffmpeg.ffprobe(this._outputs[0].target, (error, metadata) => {
       const halfway = Math.round(metadata.format.duration) / 2
       const fname = metadata.format.filename
-      cmd = ffmpeg()
-      .input(fname)
-      .inputOptions('-ss ' + halfway)
-      .outputOptions('-vf')
-      .outputOptions('thumbnail='+thumbnail_frames+',scale=480:270')
-      .outputOptions('-frames:v 1')
-      .output(changeFileEnding(fname, '.png'))
-      .on('error',(err)=>{
-        console.error(err)
-        thumbnail_frames = Math.floor(thumbnail_frames/4)
-        cmd.run()
-      })
-      .on('end', ()=>{
-        console.log("Finished Processing "+path.basename(this._outputs[0].target))
-        if(callback)
-          callback()
-      })
-      .run()
+      let thumbnail_frames = 240
+      while(true){
+        try{
+        let cmd = ffmpeg()
+          .input(fname)
+          .inputOptions('-ss ' + halfway)
+          .outputOptions('-vf')
+          .outputOptions('thumbnail='+thumbnail_frames+',scale=480:270')
+          .outputOptions('-frames:v 1')
+          .output(changeFileEnding(fname, '.png'))
+          .on('end', ()=>{
+            console.log("Finished Processing "+path.basename(this._outputs[0].target))
+            if(callback)
+              callback()
+          });
+          cmd.run()
+          break
+        }catch(err){
+          thumbnail_frames = Math.floor(thumbnail_frames/4)
+          if(thumbnail_frames<1)
+            thumbnail_frames = 1
+        }
+      }
+
+
     });
 
   })
