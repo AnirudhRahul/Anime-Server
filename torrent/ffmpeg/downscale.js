be7eb8ecf68bf59b4d65107c20046233ea804179
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-06-02 13:52:33 -0400

    Using sharp and avif for image thumbnails

--- torrent/ffmpeg/downscale.js
+++ torrent/ffmpeg/downscale.js
@@ -1,25 +1,24 @@
 ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
+
+// Downscales thumbnail to a standard 480 x 270
 module.exports.thumbnail = (metadata) =>{
-  return new Promise((resolve, reject) => {
-      const fname = friendlyName(path.basename(metadata.base_path)) + '_small.png'
-      const output_path = path.resolve(__dirname, '..','..','public','thumbnail',fname)
-      ffmpeg()
-      .input(metadata.thumbnail_path)
-      .outputOptions('-vf')
-      .outputOptions('scale=480:-1')
-      .output(output_path)
-      .once('end', ()=>{
-        console.log("Downscaled " + metadata.thumbnail_path)
-        metadata.small_thumbnail_path = path.join('/thumbnail', fname);
-        return resolve(metadata)
-      })
-      .once('error', (err, stdout, stderr)=>{
-        console.error(stderr)
-        return reject(err)
-      })
-      .run()
-  });
+  const fname = friendlyName(path.basename(metadata.base_path)) + '_small.avif'
+  const output_path = path.resolve(__dirname, '..','..','public','thumbnail',fname)
+  return sharp(metadata.thumbnail_path)
+    .resize({
+      width: 480,
+      height: 270,
+      fit: sharp.fit.cover,
+      position: sharp.strategy.entropy
+    })
+    .toFile(output_path)
+    .then(info => {
+      const bytes = info.size || 0
+      console.log("Downscaled " + metadata.thumbnail_path + " " + bytes/1024 + "Kb")
+      metadata.small_thumbnail_path = path.join('/thumbnail', fname);
+      return metadata
+    })
 }
 
 function friendlyName(filename){
--- torrent/ffmpeg/extract_thumbnail.js
+++ torrent/ffmpeg/extract_thumbnail.js
@@ -1,17 +1,20 @@
+sharp = require('sharp');
 ffmpeg = require('fluent-ffmpeg');
-const RAM_LIMIT = 1.5
+const RAM_LIMIT = 1.7
+
 function extract(metadata, seekFraction=0.75){
   //Good hueuristic for making sure the process doesnt crash because of ram usage
   const thumbnail_frames = Math.floor(RAM_LIMIT*1.25e8/(metadata.width*metadata.height))
   return new Promise((resolve, reject) => {
       const seekPoint = Math.floor(metadata.duration * seekFraction)
+      const png_path = metadata.base_path + '.png'
       ffmpeg()
       .input(metadata.video_path)
       .inputOptions('-ss ' + seekPoint)
       .outputOptions('-vf')
       .outputOptions('thumbnail=' + thumbnail_frames)
       .outputOptions('-frames:v 1')
-      .output(metadata.base_path + '.png')
+      .output(png_path)
       .once('end', (stdout, stderr)=>{
         if(stderr.includes("Output file is empty, nothing was encoded (check -ss / -t / -frames parameters if used)")){
           if(seekPoint==0){
@@ -22,8 +25,17 @@ function extract(metadata, seekFraction=0.75){
           }
         }
         console.log("Made thumbnail for " + metadata.video_path)
-        metadata.thumbnail_path = metadata.base_path + '.png'
-        return resolve(metadata)
+        sharp(png_path)
+          .toFormat('heif', { quality: 90, compression: 'av1' })
+          .toFile(metadata.base_path + '.avif')
+          .then(info => {
+            //PNG file is no longer needed
+            fs.unlinkSync(png_path)
+            metadata.thumbnail_path = metadata.base_path + '.avif'
+            const bytes = info.size || 0
+            console.log(metadata.thumbnail_path + " is " + bytes/1024 + "Kb")
+            return resolve(metadata)
+          })
       })
       .once('error', (err, stdout, stderr)=>{
         console.error(stderr)
